6a6f1bc02f9ae97605732b75799582eecb2bd388
mironov-nikita <hephaestus31415@gmail.com>
2026-03-14 13:27:28 +0200

    Create e0001-two-sum.py

---
+++ code/python/arrays/e0001-two-sum.py
@@ -0,0 +1,15 @@
+# Name: Two Sum
+# Difficulty: Easy
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# Link: https://leetcode.com/problems/two-sum/
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i in range(len(nums)):
+            seen[nums[i]] = i
+        for i in range(len(nums)):
+            y = target - nums[i]
+            if y in seen and seen[y] != i:
+                return [i, seen[y]]
